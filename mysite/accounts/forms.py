3327cba5188165acdc9e0036bcad5c983f72edf0
meia0407 <mei.ariga.0407@gmail.com>
2024-02-08 16:40:55 +0900

    change forms.py

--- mysite/accounts/forms.py
+++ mysite/accounts/forms.py
@@ -1,3 +1,28 @@
 from django import forms
 from django.contrib.auth.password_validation import validate_password
-from .models import Users
+from .models import CustomUser
+
+
+class SignInForm(forms.ModelForm):
+    email = forms.EmailField(label='メールアドレス')
+    password = forms.CharField(label='パスワード', widget=forms.PasswordInput)
+    confirm_password = forms.CharField(
+        label='確認用パスワード', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password != confirm_password:
+            raise forms.ValidationError('パスワードが一致しません。')
+
+    def save(self):
+        user = super().save(commit=False)
+        validate_password(self.cleaned_data.get('password'), user)
+        user.set_password(self.cleaned_data.get('password'))
+        user.save()
+        return user
